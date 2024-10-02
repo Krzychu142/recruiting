2244cb6866a5b6766a812983390e29b6c353c15a
Krzychu142 <kustosz142@gmail.com>
2024-10-02 19:47:46 +0200

    daily push always must be done!

--- src/main/java/com/krzysiek/recruiting/controller/AuthenticationController.java
+++ src/main/java/com/krzysiek/recruiting/controller/AuthenticationController.java
@@ -1,29 +1,19 @@
 package com.krzysiek.recruiting.controller;
 
 import com.krzysiek.recruiting.dto.RegisterRequestDTO;
-import com.krzysiek.recruiting.validator.RegisterRequestValidator;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/authentication")
 public class AuthenticationController {
-
-    private final RegisterRequestValidator registerRequestValidator;
-
-    public AuthenticationController(RegisterRequestValidator registerRequestValidator) {
-        this.registerRequestValidator = registerRequestValidator;
-    }
 
     @PostMapping("/login")
     public String login(){
@@ -31,15 +21,13 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequestDTO request, BindingResult bindingResult){
-        registerRequestValidator.validate(request, bindingResult);
-
-        if (bindingResult.hasErrors()){
-            return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequestDTO request){
+//        if (bindingResult.hasErrors()){
+////            bindingResult.getFieldError()
+//            return new ResponseEntity<>(bindingResult.getFieldError(), HttpStatus.BAD_REQUEST);
+//        }
 
         System.out.println(request);
         return new ResponseEntity<>("register", HttpStatus.OK);
     }
-
 }
--- src/main/java/com/krzysiek/recruiting/exception/GlobalException.java
+++ src/main/java/com/krzysiek/recruiting/exception/GlobalException.java
@@ -6,9 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
+@ResponseBody
 public class GlobalException {
 
     private final EmailService emailService;
