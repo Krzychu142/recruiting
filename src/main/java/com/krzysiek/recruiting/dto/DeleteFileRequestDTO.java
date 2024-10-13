f0cd78c689b03441730ae28d928bceb3f72e6bf0
Krzychu142 <kustosz142@gmail.com>
2024-10-13 18:04:43 +0200

    changed @NotBlank to @NotNull in DeleteFileRequestDTO - @NotBlank is only correct for String vlues

--- src/main/java/com/krzysiek/recruiting/dto/DeleteFileRequestDTO.java
+++ src/main/java/com/krzysiek/recruiting/dto/DeleteFileRequestDTO.java
@@ -1,11 +1,10 @@
 package com.krzysiek.recruiting.dto;
 
 import com.krzysiek.recruiting.enums.FileType;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record DeleteFileRequestDTO(
-        @NotBlank(message = "File id must be provided.")
+        @NotNull(message = "File id must be provided.")
         Long fileId,
         @NotNull(message = "File type must be provided.")
         FileType fileType
