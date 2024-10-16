0bb8df9cef053e3402ad060ebce602cecbbd5a94
Krzychu142 <kustosz142@gmail.com>
2024-10-16 22:16:52 +0200

    created CreateRecruitmentProcessRequestDTO

---
+++ src/main/java/com/krzysiek/recruiting/dto/CreateRecruitmentProcessRequestDTO.java
@@ -0,0 +1,22 @@
+package com.krzysiek.recruiting.dto;
+
+import com.krzysiek.recruiting.enums.RecruitmentProcessStatus;
+import com.krzysiek.recruiting.enums.RecruitmentTaskStatus;
+import jakarta.validation.Valid;
+
+import java.time.LocalDate;
+
+public record CreateRecruitmentProcessRequestDTO(
+        @Valid
+        JobDescriptionDTO jobDescriptionDTO,
+        Long cvId,
+        Long recruitmentTask,
+        LocalDate dateOfApplication,
+        LocalDate processEndDate,
+        Boolean hasRecruitmentTask,
+        RecruitmentTaskStatus recruitmentTaskStatus,
+        LocalDate taskDeadline,
+        LocalDate evaluationDeadline,
+        RecruitmentProcessStatus status
+)
+{}
