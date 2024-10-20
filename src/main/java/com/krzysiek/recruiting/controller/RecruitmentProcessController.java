c4aac86eb31cd2b7898aa325dcdf5a4f986b0bbd
Krzychu142 <kustosz142@gmail.com>
2024-10-20 18:34:12 +0200

    created new endpoint - getAllRecruitmentProcesses

--- src/main/java/com/krzysiek/recruiting/controller/RecruitmentProcessController.java
+++ src/main/java/com/krzysiek/recruiting/controller/RecruitmentProcessController.java
@@ -6,10 +6,7 @@ import com.krzysiek.recruiting.service.RecruitmentProcessServiceImplementation;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/recruitment")
@@ -26,4 +23,8 @@ public class RecruitmentProcessController {
         return ResponseEntity.status(HttpStatus.CREATED).body(recruitmentProcessService.createRecruitmentProcess(recruitmentProcessRequestDTO));
     }
 
+    @GetMapping
+    public ResponseEntity<BaseResponseDTO> getAllRecruitmentProcesses() {
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponseDTO("getAllRecruitmentProcesses"));
+    }
 }
